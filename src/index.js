d49e02f438c436cf5ef7663d92a2ec553210f00e
Thomas Wright <twwright@gmail.com>
2020-09-28 20:53:27 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
+document.addEventListener("DOMContentLoaded", taskSubmit());
+
+function clickInterrupt(element) {
+  document.querySelector("#")
+}
+
+function taskSubmit() {
+
+}
+
+
+
+
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  document.querySelector("#submit-task").addEventListener(
+    "click",
+    function (e) {
+      e.preventDefault();
+      const task = document.getElementById("new-task-description").value;
+      const list = document.getElementById("list");
+      const addTask = document.createElement("li");
+      addTask.appendChild(document.createTextNode(task));
+      list.appendChild(addTask);
+    },
+    false
+  );
+
+  
 });
