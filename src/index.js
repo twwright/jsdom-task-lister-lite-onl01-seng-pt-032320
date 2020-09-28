a9e00b8314a732a921ea24f7510feca11f95e2ed
Thomas Wright <twwright@gmail.com>
2020-09-28 21:16:56 +0000

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  
+  const taskForm = document.getElementById("create-task-form");
+  const taskDesc = document.getElementById("new-task-description");
+  
+  taskForm.addEventListener("submit", () => addNewItem(event, taskDesc));
 });
+
+function addNewItem(event, taskDesc) {
+  event.preventDefault();
+  
+  const newItem = document.createElement("li");
+  
+  newItem.innerText = taskDesc.value;
+  document.getElementById("tasks").appendChild(newItem);
+}
