ad366096b50ecc64340bb377a3526f0eeb6c8096
Thomas Wright <twwright@gmail.com>
2020-09-28 21:11:58 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 document.addEventListener("DOMContentLoaded", () => {
+  
   const taskForm = document.getElementById("create-task-form");
   const taskDesc = document.getElementById("new-task-description");
   const addTaskItem = document.getElementById("tasks");
@@ -8,41 +9,9 @@ document.addEventListener("DOMContentLoaded", () => {
 
 function addNewItem(event, taskDesc) {
   event.preventDefault();
+  
   const newItem = document.createElement("li");
+  
   newItem.innerText = taskDesc.value;
   addTaskItem.appendChild(newItem);
 }
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-  //grab all the necessary DOM elements
-
-  //form and relevant input fields
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
-  const newTaskUl = document.getElementById("tasks");
-
-  //attach event listeners
-  newTaskForm.addEventListener("submit", () =>
-    createNewTask(event, newTaskDescription)
-  );
-});
-
-const createNewTask = (event, newTaskDescription) => {
-  //stop form from trying to submit
-  event.preventDefault();
-
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
-
-  appendNewTask(newTask);
-};
-
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
