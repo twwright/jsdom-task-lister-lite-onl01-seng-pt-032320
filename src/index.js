04a71aecd83873e1960c987c8daecef571c3d839
Thomas Wright <twwright@gmail.com>
2020-09-28 21:02:16 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,35 +1,37 @@
-document.addEventListener("DOMContentLoaded", taskSubmission());
+document.addEventListener("DOMContentLoaded", () => {
+  const task = document.getElementById("new-task-description").value;
+  const list = document.getElementById("list");
+  const newTaskElement = document.createElement("li");
 
-const task = document.getElementById("new-task-description").value;
-const list = document.getElementById("list");
-const newTaskElement = document.createElement("li");
+  function taskSubmission(element) {
+    document.querySelector(`${element}`).addEventListener("click", createTask(event), false);
+  }
 
-function taskSubmission(element) {
-  document.querySelector(`${element}`).addEventListener("click", createTask(event), false);
-}
+  function createTask(event) {
+    event.preventDefault();
+    newTaskElement.appendChild(document.createTextNode(task));
+    list.appendChild(taskElement);
+  }
 
-function createTask(event) {
-  event.preventDefault();
-  newTaskElement.appendChild(document.createTextNode(task));
-  list.appendChild(taskElement);
-}
+});
 
 
+document.addEventListener("DOMContentLoaded", () => {
+  // your code here
 
+  const taskForm = document.querySelector("#create-task-form")
+   const tasklist = document.querySelector("#tasks")
+  taskForm.addEventListener("submit", function(c){
+       c.preventDefault()
+      const newTask = document.querySelector("#new-task-description").value
+     const taskItem = document.createElement("li")
+     taskItem.innerText = newTask
 
-// document.addEventListener("DOMContentLoaded", () => {
-//   document.querySelector("#submit-task").addEventListener(
-//     "click",
-//     function (e) {
-//       e.preventDefault();
-//       const task = document.getElementById("new-task-description").value;
-//       const list = document.getElementById("list");
-//       const addTask = document.createElement("li");
-//       addTask.appendChild(document.createTextNode(task));
-//       list.appendChild(addTask);
-//     },
-//     false
-//   );
+     tasklist.appendChild(taskItem)
+     taskForm.reset()
 
+     
+      
+  })
   
-// });
+});
