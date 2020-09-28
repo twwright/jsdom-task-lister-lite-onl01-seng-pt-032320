bb22b6d521e6d2eb8f5ea6fa9fd6738083091b06
Thomas Wright <twwright@gmail.com>
2020-09-28 20:57:21 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -21,19 +21,19 @@ function createTask(el) {
 
 
 
-document.addEventListener("DOMContentLoaded", () => {
-  document.querySelector("#submit-task").addEventListener(
-    "click",
-    function (e) {
-      e.preventDefault();
-      const task = document.getElementById("new-task-description").value;
-      const list = document.getElementById("list");
-      const addTask = document.createElement("li");
-      addTask.appendChild(document.createTextNode(task));
-      list.appendChild(addTask);
-    },
-    false
-  );
+// document.addEventListener("DOMContentLoaded", () => {
+//   document.querySelector("#submit-task").addEventListener(
+//     "click",
+//     function (e) {
+//       e.preventDefault();
+//       const task = document.getElementById("new-task-description").value;
+//       const list = document.getElementById("list");
+//       const addTask = document.createElement("li");
+//       addTask.appendChild(document.createTextNode(task));
+//       list.appendChild(addTask);
+//     },
+//     false
+//   );
 
   
-});
+// });
