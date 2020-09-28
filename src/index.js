6ab5965d89a3530b7e24af2e6c5f11e1a02fe09a
Thomas Wright <twwright@gmail.com>
2020-09-28 21:13:17 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,43 +1,36 @@
-// document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => {
   
-//   const taskForm = document.getElementById("create-task-form");
-//   const taskDesc = document.getElementById("new-task-description");
-//   const addTaskItem = document.getElementById("tasks");
+  const taskForm = document.getElementById("create-task-form");
+  const taskDesc = document.getElementById("new-task-description");
+  const addTaskItem = document.getElementById("tasks");
   
-//   taskForm.addEventListener("submit", () => addNewItem(event, taskDesc));
-// });
+  taskForm.addEventListener("submit", () => addNewItem(event, taskDesc));
+});
 
-// function addNewItem(event, taskDesc) {
-//   event.preventDefault();
+function addNewItem(event, taskDesc) {
+  event.preventDefault();
   
-//   const newItem = document.createElement("li");
+  const newItem = document.createElement("li");
   
-//   newItem.innerText = taskDesc.value;
-//   addTaskItem.appendChild(newItem);
-// }
+  newItem.innerText = taskDesc.value;
+  addTaskItem.appendChild(newItem);
+}
 
 
 
 
 document.addEventListener("DOMContentLoaded", () => {
-  //grab all the necessary DOM elements
 
-  //form and relevant input fields
   const newTaskForm = document.getElementById("create-task-form");
   const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskPriority = document.getElementById("new-task-priority");
-
-  //ul where new tasks will live on the DOM
   const newTaskUl = document.getElementById("tasks");
 
-  //attach event listeners
   newTaskForm.addEventListener("submit", () =>
     createNewTask(event, newTaskDescription)
   );
 });
 
 const createNewTask = (event, newTaskDescription) => {
-  //stop form from trying to submit
   event.preventDefault();
 
   const newTask = document.createElement("li");
