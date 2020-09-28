e7affc865a58471432d904d18c56d43856d3608b
Thomas Wright <twwright@gmail.com>
2020-09-28 21:15:15 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ document.addEventListener("DOMContentLoaded", () => {
   
   const taskForm = document.getElementById("create-task-form");
   const taskDesc = document.getElementById("new-task-description");
-  const addTaskItem = document.getElementById("tasks");
   
   taskForm.addEventListener("submit", () => addNewItem(event, taskDesc));
 });
@@ -13,32 +12,5 @@ function addNewItem(event, taskDesc) {
   const newItem = document.createElement("li");
   
   newItem.innerText = taskDesc.value;
-  addTaskItem.appendChild(newItem);
+  document.getElementById("tasks").appendChild(newItem);
 }
-
-
-
-
-document.addEventListener("DOMContentLoaded", () => {
-
-  const newTaskForm = document.getElementById("create-task-form");
-  const newTaskDescription = document.getElementById("new-task-description");
-  const newTaskUl = document.getElementById("tasks");
-
-  newTaskForm.addEventListener("submit", () =>
-    createNewTask(event, newTaskDescription)
-  );
-});
-
-const createNewTask = (event, newTaskDescription) => {
-  event.preventDefault();
-
-  const newTask = document.createElement("li");
-  newTask.innerText = newTaskDescription.value;
-
-  appendNewTask(newTask);
-};
-
-const appendNewTask = task => {
-  document.getElementById("tasks").appendChild(task);
-};
